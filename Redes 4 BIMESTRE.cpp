1af3638fd46aa13f8172ba50c044400685628896
Rodrigo Lopes Marques <rodrigolopes.m2005@gmail.com>
2021-10-24 20:13:09 -0300

    Update Redes 4 BIMESTRE.cpp

--- Redes 4 BIMESTRE.cpp
+++ Redes 4 BIMESTRE.cpp
@@ -2,68 +2,119 @@
 #include <string.h>
 #include <ctype.h>
 #include <stdlib.h>
+#include <locale.h>
 
     int n, i, key, x;
-	char c, msg[100];
+	char c, msg[50];
 	
 
 main()
 {
-
-	printf("=-=-=-=-=-=-=-=-=-=-=\n");
-	printf("Criptografia de César\n");	
-	printf("=-=-=-=-=-=-=-=-=-=-=\n\n");
+	setlocale(LC_ALL, "Portuguese");
 	
-	printf("[1] - Criptografar um texto\n");
-	printf("[2] - Descriptografar um texto\n");
-	printf("[0] - Encerrar\n\n");
-	
-	scanf("%d", &x);
-	
-	switch(x){
+	do{
 		
-		case 1:
+		system("cls");
+		printf("=-=-=-=-=-=-=-=-=-=-=\n");
+		printf("Criptografia de César\n");	
+		printf("=-=-=-=-=-=-=-=-=-=-=\n\n");
+		
+		printf("[1] - Criptografar um texto\n");
+		printf("[2] - Descriptografar um texto\n");
+		printf("[0] - Encerrar\n\n");
+		
+		printf("-> ");
+		scanf("%d", &x);
+		
+		switch(x){
 			
-			system("cls");
-			printf("Texto: ");
-		    gets(msg);
-			fflush(stdin);
+			case 1:
+				
+				system("cls");
+				
+				printf("Texto: ");
+				fflush(stdin);
+			    gets(msg);
+			    
+			    printf("\nKey: ");
+			    scanf("%d", &key);
+			    
+			    printf("\nTexto Criptografado: ");
+			    n = strlen(msg);
 			
-			printf("\n%s\n", msg);
-		    
-		    printf("\nKey: ");
-		    scanf("%d", &key);
-		    
-		    printf("\nTexto Criptografado: ");
-		    n = strlen(msg);
+			    for (i = 0; i < n; i++)
+			    {
+			        if(msg[i] == '\0')
+			        {
+			            printf(" ");
+			        }
+			        else if(isalpha(msg[i]) == 0)
+			        {
+			        	c = msg[i];
+			            printf("%c", c);
+			        }
+			        else if(islower(msg[i]) == 0)
+			        {
+			            c = ((msg[i] - 65) + key) % 26;
+			            printf("%c", c + 65);
+			        }
+			        else if(isupper(msg[i]) == 0)
+			        {
+			            c = ((msg[i] - 97) + key) % 26;
+			            printf("%c", c + 97);
+			        }
+			    }
+			    
+			    printf("\n\n");
+			    system("pause");
+				break;
+				
+			case 2:
+				
+				system("cls");
+				
+				printf("Texto: ");
+				fflush(stdin);
+			    gets(msg);
+			    
+			    printf("\nKey: ");
+			    scanf("%d", &key);
+			    
+			    printf("\nTexto Descriptografado: ");
+			    n = strlen(msg);
+			
+			    for (i = 0; i < n; i++)
+			    {
+			        if(msg[i] == '\0')
+			        {
+			            printf(" ");
+			        }
+			        else if(isalpha(msg[i]) == 0)
+			        {
+			        	c = msg[i];
+			            printf("%c", c);
+			        }
+			        else if(islower(msg[i]) == 0)
+			        {
+			            c = ((msg[i] - 65) - key) % 26;
+			            printf("%c", c + 65);
+			        }
+			        else if(isupper(msg[i]) == 0)
+			        {
+			            c = ((msg[i] - 97) - key) % 26;
+			            printf("%c", c + 97);
+			        }
+			    }
+			    printf("\n\n");
+			    system("pause");
+				break;			
+			
+			
+			default:
+				x = 0;
+				break;			
+		}
 		
-		    for (i = 0; i < n; i++)
-		    {
-		        if(msg[i] == 32)
-		        {
-		            printf(" ");
-		        }
-		        else if(isalpha(msg[i]) == 0)
-		        {
-		            printf("%c", c);
-		        }
-		        else if(islower(msg[i]) == 0)
-		        {
-		            c = ((msg[i] - 65) + key) % 26;
-		            printf("%c", c + 65);
-		        }
-		        else if(isupper(msg[i]) == 0)
-		        {
-		            c = ((msg[i] - 97) + key) % 26;
-		            printf("%c", c + 97);
-		        }
-		    }
-		    getchar();
-			break;
-		
-		
-		default:
-			x = 0;
-			break;			
-	}
+	}while (x > 0);
+	
 }
